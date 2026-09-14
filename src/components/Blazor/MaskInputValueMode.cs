4125741841f6d04a2a09705b8580da7825af78a0
Damyan Petev <damyanpetev@users.noreply.github.com>
2026-09-14 13:14:05 +0300

    Apply suggestion from @damyanpetev

--- src/components/Blazor/MaskInputValueMode.cs
+++ src/components/Blazor/MaskInputValueMode.cs
@@ -5,7 +5,7 @@ namespace IgniteUI.Blazor.Controls
     /// </summary>
     public enum MaskInputValueMode
     {
-        /// <summary>Returns clean input , e.g. <c>5551234567</c>.</summary>
+        /// <summary>Returns clean input, e.g. <c>5551234567</c>.</summary>
         Raw,
         /// <summary>Returns with mask formatting, e.g. <c>(555) 123-4567</c>.</summary>
         WithFormatting
