68548c8f182f0628e8369380806c51942946dbbc
damyanpetev <damyanpetev@users.noreply.github.com>
2026-09-17 19:51:24 +0300

    refactor: hide the render hooks of the base classes

--- src/components/Blazor/DateTimeInput.cs
+++ src/components/Blazor/DateTimeInput.cs
@@ -21,13 +21,13 @@ namespace IgniteUI.Blazor.Controls
         }
 
         /// <inheritdoc />
-        protected override string ResolveDisplay()
+        private protected override string ResolveDisplay()
         {
             return "inline-block";
         }
 
         /// <inheritdoc />
-        protected override bool SupportsVisualChildren
+        private protected override bool SupportsVisualChildren
         {
             get
             {
