736387b3872b6b3ecd7cfe1bdfef712854eee2ab
Damyan Petev <damyanpetev@users.noreply.github.com>
2026-09-21 18:16:52 +0300

    refactor: make ToEventJson/FromEventJson internal (#448)

--- src/components/Blazor/ComponentBoolValueChangedEventArgs.cs
+++ src/components/Blazor/ComponentBoolValueChangedEventArgs.cs
@@ -41,7 +41,7 @@ namespace IgniteUI.Blazor.Controls
         }
 
         /// <inheritdoc />
-        protected internal override void ToEventJson(BaseRendererControl control, Dictionary<string, object?> args)
+        internal override void ToEventJson(BaseRendererControl control, Dictionary<string, object?> args)
         {
             base.ToEventJson(control, args);
 
@@ -51,7 +51,7 @@ namespace IgniteUI.Blazor.Controls
         }
 
         /// <inheritdoc />
-        protected internal override void FromEventJson(BaseRendererControl control, Dictionary<string, object?>? args)
+        internal override void FromEventJson(BaseRendererControl control, Dictionary<string, object?>? args)
         {
             base.FromEventJson(control, args);
             this.SuppressParentNotify = true;
